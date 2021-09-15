9415718d3d5d16d89bbaca1c46e39a69a7a700e2
Joseph Jarman <57240500+Jarmander333@users.noreply.github.com>
2021-09-14 22:31:17 -0500

    Update 3.1_Temperature.py

--- 3.1_Temperature.py
+++ 3.1_Temperature.py
@@ -9,10 +9,11 @@ In: 32  Out: 0
 In: 212  Out: 100
 In: 52  Out: 11.1
 In: 25  Out: -3.9
-In: -40  Out: ??? Please tell me what this output is!
+In: -40  Out: -40 Please tell me what this output is!
 
 '''
 
-
-
-
+temp = float(input("Please input a fahrenheit temperature: "))
+F = temp - 32
+C = F * 5/9
+print(str(temp) + " Fahrenheit is " + str(C) + " Celsius!")
