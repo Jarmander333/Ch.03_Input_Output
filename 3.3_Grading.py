a242445b1250dda241655def436770f2b7e28056
Joseph Jarman <57240500+Jarmander333@users.noreply.github.com>
2021-09-15 12:51:46 -0500

    Update 3.3_Grading.py

--- 3.3_Grading.py
+++ 3.3_Grading.py
@@ -1,10 +1,5 @@
 '''
-Grading PROGRAM
----------------
-Create a program that asks the user for their semester grade, final exam grade, 
-and final exam worth and then calculates the overall final grade. 
-Ask your instructor if you don't know how to calculate weighted averages.
-You don't have to print out the letter grade. We will do that in the next chapter.
+GRADING PROGRAM
 
 Test with the following:
 
@@ -13,28 +8,22 @@ Sem Grade: 95   Final Exam: 32   Exam worth: 10%    Overall: 88.7
 Sem Grade: 72   Final Exam: 100   Exam worth: 20%    Overall: 77.6
 '''
 
-sem_grade = float(input("Please input your semester grade excluding the final exam: "))
-exam_grade = float(input("Please input your overall exam grade: "))
-exam_valu_deci = float(input("Please input your overall exam worth (0 - 100): "))
-exam_valu = exam_valu_deci * 0.01
-sem_valu = 1.00 - exam_valu
-sem_average = sem_grade * sem_valu + exam_grade * exam_valu
-
-print("Your semester average is " + str(sem_average))
-
 '''
-if sem_average >= 98:
-    print("You got an A+!")
-elif 97 >= sem_average >= 94:
-    print("You got an A!")
-elif 90 >= sem_average >= 93:
-    print("You got an A-!")
-elif 87 >= sem_average >= 89:
-    print("You got a B+!")
-elif 83 >= sem_average >= 86:
-    print("You got a B!")
-elif 80 >= sem_average >= 82:
-    print("You got a B-!")
-else:
-    print("You got a F!")
+sem_grade: the overall semester grade
+sem_value: how much the semester was worth
+exam_grade: the overall exam grade
+exam_value: how much the exam was worth
 '''
+sem_grade = float(input("Please input your semester grade excluding the final exam: ")) # Asks for the semester grade
+exam_grade = float(input("Please input your overall exam grade: ")) # Asks for the exam grade
+exam_value = float(input("Please input your overall exam worth: ")) # Asks how much the exam was worth
+
+# Checks is the user's exam value is a decimal, or not.
+if 1 <= exam_value <= 100: # Checks if 'exam_value' is > 1 and < 100
+    exam_value = exam_value * 0.01 # If it is, change it to decimal form
+# !!! If none of this applies, it just moves on !!!
+
+sem_value = 1.00 - exam_value # Find how much the semester was worth based on the exam value
+sem_average = sem_grade * sem_value + exam_grade * exam_value # Calculate the average
+
+print("Your semester average is " + str(sem_average)) # Prints the average
